b19d540dcb2bfa22fbb6a53bf3a3df838dcc8e43
MixidFinder <mixidfinder@gmail.com>
2022-06-22 22:24:45 +0300

    lesson-1

--- app/src/main/java/com/mixfin/android/MainActivity.java
+++ app/src/main/java/com/mixfin/android/MainActivity.java
@@ -2,13 +2,30 @@ package com.mixfin.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button switchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchButton = findViewById(R.id.switchButton);
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, SecondActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
